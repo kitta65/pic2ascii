51f78b35f1786caa609bf9f51b1116945da94148
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-07-21 19:22:42 +0900

    use stb_image instead of CImg

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-default: libraries/CImg.h
+default: out/main
 
-libraries/CImg.h:
-	curl -o libraries/CImg.h https://raw.githubusercontent.com/GreycLab/CImg/v.3.4.0/CImg.h
+libraries/stb_image.h:
+	curl -o libraries/stb_image.h https://raw.githubusercontent.com/nothings/stb/f7f20f39fe4f206c6f19e26ebfef7b261ee59ee4/stb_image.h
+
+out/main: libraries/stb_image.h main.cpp
+	g++ -o out/main main.cpp
--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-#include "./libraries/CImg.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "./libraries/stb_image.h"
 
 using std::cout, std::endl;
 
-int main() { cout << "hello world" << endl; }
+int main() {
+  int x, y, n;
+  unsigned char *data = stbi_load("./pictures/black.png", &x, &y, &n, 4);
+  cout << x << endl;
+  cout << y << endl;
+  cout << n << endl;
+  stbi_image_free(data);
+}
